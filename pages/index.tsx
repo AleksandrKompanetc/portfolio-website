ad48fb5f491e85fbcf4294404260e6cefa47bcbe
AleksandrKompanetc <moneymacker7@gmail.com>
2024-09-23 23:51:11 +0300

    creating component Nav

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Nav from '@/Components/Nav'
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div className='overflow-x-hidden'>
+      <div>
+        <Nav />
+      </div>
+    </div>
   )
 }
 
