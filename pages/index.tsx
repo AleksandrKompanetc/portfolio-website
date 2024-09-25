ae100961cbb11a9a9c06b1506e1353fbbcb8fd1f
AleksandrKompanetc <moneymacker7@gmail.com>
2024-09-25 23:50:55 +0300

    adding states to HomePage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Nav from '@/Components/Nav'
 
 const HomePage = () => {
+  const [nav, setNav] = useState(false)
+  const openNav = () => setNav(true)
+  const closeNav = () => setNav(false)
+  
   return (
     <div className='overflow-x-hidden'>
       <div>
