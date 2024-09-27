51f0c26b7a2ba97b8d44d7e1f62b24248f39e20b
AleksandrKompanetc <moneymacker7@gmail.com>
2024-09-27 21:14:51 +0300

    adding components About and Services

--- Components/Hero.tsx
+++ Components/Hero.tsx
@@ -6,7 +6,7 @@ import { ArrowDownTrayIcon, PlayCircleIcon } from '@heroicons/react/20/solid'
 
 const Hero = (): any => {
   return (
-    <div className="h-[88vh] bg-[url('/images/fon3.jpg')] bg-cover bg-center">
+    <div className="h-[88vh] bg-[url('/images/fon3.jpg')] mt-[10vh] bg-cover bg-center">
       <Particle />
       <div className='w-[80%] grid-cols-1 mx-auto grid lg:grid-cols-2 gap-[3rem] h-[100%] items-center'>
         <div className=''>
