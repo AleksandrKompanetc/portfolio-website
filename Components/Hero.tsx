eafaab76368de244383001a8c3f448ba0da02b93
AleksandrKompanetc <moneymacker7@gmail.com>
2024-09-27 15:10:01 +0300

    adding Particle

---
+++ Components/Hero.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Particle from './Particle'
+
+const Hero = () => {
+  <div className="h-[88vh] bg-[url('/images/img.png')] bg-cover bg-center">
+    <Particle />
+  </div>
+}
+
+export default Hero
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,21 @@
 import React, {useState} from 'react'
 import Nav from '@/Components/Nav'
+import MobileNav from '@/Components/MobileNav'
+import Hero from '@/Components/Hero'
 
 const HomePage = () => {
   const [nav, setNav] = useState(false)
   const openNav = () => setNav(true)
   const closeNav = () => setNav(false)
-  
+
   return (
     <div className='overflow-x-hidden'>
       <div>
-        <Nav />
+        {/* NavBar */}
+        <MobileNav nav={nav} closeNav={closeNav} />
+        <Nav openNav={openNav} />
+        {/* Hero */}
+        <Hero />
       </div>
     </div>
   )
