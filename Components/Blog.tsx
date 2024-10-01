aa1aba2466a1474925147a45007d045a16fac610
AleksandrKompanetc <moneymacker7@gmail.com>
2024-10-02 00:13:10 +0300

    changing component Blog

--- Components/Blog.tsx
+++ Components/Blog.tsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <div className='pt-[4rem] md:pt-[8rem] pb-[4rem] bg-[#02050a'>
+      <h1 className='heading'>
+        MY
+        <span className='text-yellow-400'> BLOG</span>
+      </h1>
+      <div className=''></div>
+    </div>
   )
 }
 
