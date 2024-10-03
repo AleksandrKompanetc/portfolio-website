7faf350849393e6b55015c9b47f30d843bf9d4b1
AleksandrKompanetc <moneymacker7@gmail.com>
2024-10-03 22:58:14 +0300

    adding animations

--- Components/Projects.tsx
+++ Components/Projects.tsx
@@ -8,7 +8,7 @@ const Projects = () => {
         Pro<span className='text-yellow-400'>Ject</span>
       </h1>
       <div className='w-[80%] pt-[2rem] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-[2rem]'>
-        <div>
+        <div data-aos='fade-up'>
           <div className='transform cursor-pointer hover:-translate-y-6 transition-all duration-200 
           relative w-[100%] h-[200px] md:h-[300px]'>
             <Image 
@@ -19,7 +19,7 @@ const Projects = () => {
             />
           </div>
         </div>
-        <div>
+        <div data-aos='fade-up' data-aos-delay='300'>
           <div className='transform cursor-pointer hover:-translate-y-6 transition-all duration-200 
           relative w-[100%] h-[200px] md:h-[300px]'>
             <Image 
@@ -30,7 +30,7 @@ const Projects = () => {
             />
           </div>
         </div>
-        <div>
+        <div data-aos='fade-up' data-aos-delay='600'>
           <div className='transform cursor-pointer hover:-translate-y-6 transition-all duration-200 
           relative w-[100%] h-[200px] md:h-[300px]'>
             <Image 
@@ -41,7 +41,7 @@ const Projects = () => {
             />
           </div>
         </div>
-        <div>
+        <div data-aos='fade-up' data-aos-delay='900'>
           <div className='transform cursor-pointer hover:-translate-y-6 transition-all duration-200 
           relative w-[100%] h-[200px] md:h-[300px]'>
             <Image 
@@ -52,7 +52,7 @@ const Projects = () => {
             />
           </div>
         </div>
-        <div>
+        <div data-aos='fade-up' data-aos-delay='1200'>
           <div className='transform cursor-pointer hover:-translate-y-6 transition-all duration-200 
           relative w-[100%] h-[200px] md:h-[300px]'>
             <Image 
@@ -63,7 +63,7 @@ const Projects = () => {
             />
           </div>
         </div>
-        <div>
+        <div data-aos='fade-up' data-aos-delay='1600'>
           <div className='transform cursor-pointer hover:-translate-y-6 transition-all duration-200 
           relative w-[100%] h-[200px] md:h-[300px]'>
             <Image 
