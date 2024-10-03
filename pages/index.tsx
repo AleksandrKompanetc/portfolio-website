8fe760cf87be0af93033be167e677d6279818ae0
AleksandrKompanetc <moneymacker7@gmail.com>
2024-10-03 22:25:23 +0300

    adding component Footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,7 @@ import Skills from '@/Components/Skills';
 import Projects from '@/Components/Projects';
 import Testimonial from '@/Components/Testimonal';
 import Blog from '@/Components/Blog';
+import Footer from '@/Components/Footer';
 
 const HomePage = () => {
   const [nav, setNav] = useState(false)
@@ -29,6 +30,7 @@ const HomePage = () => {
           <Projects />
           <Testimonial />
           <Blog />
+          <Footer />
         </div>
       </div>
     </div>
